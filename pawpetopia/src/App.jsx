9bb94c9dc529c71ba1e6cc15cf475fb41e1b5476
ShreyasNarlawar <narlawarshreyas@gmail.com>
2024-12-15 17:52:36 +0530

    1512_1

--- pawpetopia/src/App.jsx
+++ pawpetopia/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './Component/NavBar/NavBar';
 import Home from './Component/Home/Home'; // Home component
 import Login from './Component/Register-Login/Login';
@@ -12,31 +12,31 @@ import FAQ from './Component/FAQ/FAQ'; // FAQ component
 import Terms from './Component/Terms/Terms'; // Terms of Service component
 import Footer from './Component/Footer/Footer'; // Footer component
 import NotFound from './Component/NotFound/NotFound'; // 404 Not Found component
-import Stats from './Component/Stats/Stats'
-;
+import Stats from './Component/Stats/Stats'; 
 import './App.css'; 
+
 const App = () => {
   return (
-    <div>
-      <Router>
-      <div className="app-container"> {/* Added a container for better styling */}
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/adopt" element={<Adopt />} />
-          <Route path="/donate" element={<Donate />} />
-          <Route path="/aboutUs" element={<AboutUs />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="*" element={<NotFound />} /> {/* Catch-all route for 404 */}
-        </Routes>
-        {/* <Footer /> */}
-        </div>
-      </Router>
-    </div>
+    <Router> 
+      <div className="app-container"> 
+        <NavBar /> 
+        <main> {/* Wrap the Routes within a <main> tag */}
+          <Routes>
+            <Route path="/" element={<Home />} /> 
+            <Route path="/adopt" element={<Adopt />} />
+            <Route path="/donate" element={<Donate />} />
+            <Route path="/aboutUs" element={<AboutUs />} /> 
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="*" element={<NotFound />} /> 
+          </Routes>
+        </main>
+       
+      </div>
+    </Router>
   );
 };
 
